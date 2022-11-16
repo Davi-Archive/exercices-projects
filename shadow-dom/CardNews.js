ac1ba3a0e75751af14942f47a64ec2d9e45c7f22
d1av <davi4alves@gmail.com>
2022-11-16 12:05:27 -0300

    feat: add props to component
    created new dom in class
    js tree

---
+++ shadow-dom/CardNews.js
@@ -0,0 +1,48 @@
+class CardNews extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
+
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__left");
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__right");
+
+    const autor = document.createElement("span");
+    const linkTitle = document.createElement("a");
+    const newsContent = document.createElement("p");
+
+    linkTitle.href = this.getAttribute("link-url");
+
+    autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+    linkTitle.textContent = this.getAttribute("title");
+    newsContent.textContent = this.getAttribute("content");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
+    const newsImage = document.createElement("img");
+    cardRight.appendChild(newsImage);
+    newsImage.src = this.getAttribute("img-url")||"default.png";
+    newsImage.alt = "Foto da Notícia";
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+  }
+
+  styles() {}
+}
+
+customElements.define("card-news", CardNews);
--- shadow-dom/index.js
+++
@@ -1,9 +0,0 @@
-class CardNews extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = "<h1>Davi</h1>";
-  }
-}
-
-customElements.define("card-news", CardNews);
